aeae91c21daeb884d49f2eb254adefdc9d54939e
coelhos-gabi <coelhos.gabi@gmail.com>
2022-05-06 19:22:07 -0300

    Criação dos métodos para realizar venda e imprimir relatório de vendas

---
+++ src/br/com/gabrielacoelho/StringContains.java
@@ -0,0 +1,11 @@
+package br.com.gabrielacoelho;
+
+import java.util.Locale;
+
+public class StringContains {
+    public static void main(String[] args) {
+        String nome = "Ana";
+        String nome2 = "Juliana";
+        System.out.println(nome2.contains(nome.toLowerCase()));
+    }
+}
---
+++ src/br/com/gabrielacoelho/TipoClientes.java
@@ -0,0 +1,21 @@
+package br.com.gabrielacoelho;
+
+public enum TipoClientes {
+    FISICA("PF", 1),
+    JURIDICA("PJ", 0.95),
+    VIP("VIP", 0.85);
+    private double desconto;
+    private String tipo;
+    TipoClientes(String descricao, double desconto){
+        this.tipo = descricao;
+        this.desconto = desconto;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public double getDesconto() {
+        return this.desconto;
+    }
+}
